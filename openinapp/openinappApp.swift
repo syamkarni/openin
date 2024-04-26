efd67158b1b3d875daea7781fada06f74ccf1d1e
Syamkarni Uppalapati <101443460+syamkarni@users.noreply.github.com>
2024-04-27 02:18:14 +0530

    Initial Commit

---
+++ openinapp/openinappApp.swift
@@ -0,0 +1,17 @@
+//
+//  openinappApp.swift
+//  openinapp
+//
+//  Created by syamkarni uppalapati on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct openinappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
